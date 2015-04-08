e71664356d8646dcf822db44e65d9be680a92c35
Marc Mentis <mmentis@optonline.net>
2015-04-08 11:56:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestDeploysuite2::Application.config.secret_key_base = '2215fb0546b7700f7144d5a5f3cfc275845c5dc89944a6e316c956702093128fbcda4fea0911f22cafa867d8ffddc5cf229d1a92bcc4a11bb4b5ae90880c8dfa'
